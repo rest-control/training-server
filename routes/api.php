6ff3260395a337757fa5b4b0cd2e682f0788472c
kamszel <info@kamilszela.pl>
2018-04-14 11:12:25 +0200

    prepare endpoint with files to download

--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,25 @@ Route::get('oauth2/users', function(){
         ->json(\DB::table('users')->select()->limit(20)->get());
 
 })->middleware('auth:api');
+
+Route::get('plain/files/{type}', function($type) {
+
+    $types = [
+        'csv' => 'text/csv',
+        'css' => 'text/css',
+        'js'  => 'text/javascript',
+        'png' => 'image/png',
+    ];
+
+    if(!isset($types[$type])) {
+        throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException();
+    }
+
+    return response()->download(
+        base_path() . '/resources/assets/sample-files/sample.' . $type,
+        'sample.' .  $type,
+        [
+            'Content-Type' => array_get($types, $type),
+        ]
+    );
+});
